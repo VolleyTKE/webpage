23c05f0e084c530098433b94f49e6f9219db2091
Chris Carr <chris.a.carr@gmail.com>
2015-09-09 06:52:41 -0500

    add final updates mostly completed controller and completed web page

--- controllers.js
+++ controllers.js
@@ -1,12 +1,45 @@
-var pantryItemApp = angular.module('pantryItemApp', []);
 
-pantryItemApp.controller('ItemManagerCtrl', ['$scope','$http', function ($scope,$http) {
+
+/* Controllers */
+var App = angular.module('App', []);
+
+App.controller('ItemManagerCtrl', ['$scope','$http', function ($scope,$http) {
+	$scope.item = {};
+	//get all tasks
+	
 	$http.get('http://localhost:8001/api/getAllItems').success(function(data) {
 	$scope.items = data;});	
 	$scope.name = "World";
+	
   
-    
+    //add a new task
+	
+	
+ $scope.addItem = function addItem() {
+	 console.log("I was hit!");
+  
+   $http.post('http://localhost:8001/api/addItem', {
+             upc: $scope.upc,
+             itemName: $scope.itemName,
+             company: $scope.company,
+			 dateEntered: $scope.dateEntered,
+			 expirationDate: $scope.expirationDate
+             
+         }).
+    success(function(data, status, headers) {
+		$scope.item = {}; 
+		$scope.items.push(data);
+    console.log("Task added");
+             //var newTaskUri = headers()["location"];
+             //console.log("Might be good to GET " + newTaskUri + " and append the task.");
+             // Refetching EVERYTHING every time can get expensive over time
+             // Better solution would be to $http.get(headers()["location"]) and add it to the list
+             //findAllTasks();
+	});
+  
+  
+ };
   
   $scope.orderProp = 'expirationDate';
-});
+}]);
 
