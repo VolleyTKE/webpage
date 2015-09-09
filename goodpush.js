cec55620a4f8db5f70cb7ca74971b5fc502998f0
Chris Carr <chris.a.carr@gmail.com>
2015-09-08 20:23:29 -0500

    add $http post and form for data entry

--- goodpush.js
+++ goodpush.js
@@ -8,7 +8,30 @@ App.controller('ItemManagerCtrl', ['$scope','$http', function ($scope,$http) {
 	$scope.items = data;});	
 	$scope.name = "World";
   
-    
+    //add a new task
+ $scope.addItem = function addItem() {
+	 console.log("I was hit!");
+  if($scope.upc=="" || $scope.itemName=="" || $scope.company == "" ){
+   alert("Insufficient Data! Please provide values for task name, description, priortiy and status");
+  }
+  else{
+   $http.post('http://localhost:8001/api/addItem', {
+             upc: $scope.upc,
+             itemName: $scope.itemName,
+             company: $scope.company
+             
+         }).
+    success(function(data, status, headers) {
+    console.log("Task added");
+             //var newTaskUri = headers()["location"];
+             //console.log("Might be good to GET " + newTaskUri + " and append the task.");
+             // Refetching EVERYTHING every time can get expensive over time
+             // Better solution would be to $http.get(headers()["location"]) and add it to the list
+             //findAllTasks();
+      });
+  }
+ };
   
   $scope.orderProp = 'expirationDate';
 }]);
+
